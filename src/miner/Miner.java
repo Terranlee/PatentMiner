05229ab75cbd37a33953b8747451100f15200b46
terranlee <ltrthu@163.com>
2015-03-19 18:27:07 +0800

    solve running in linux

--- src/miner/Miner.java
+++ src/miner/Miner.java
@@ -2,14 +2,12 @@ package miner;
 
 import keyword.KeywordExtract;
 
+
 public class Miner {
 	public static void main(String[] args){
 		KeywordExtract.test();
-	//	导入用户词个数80158
-	//	:盐碱尘暴,研究,艾比湖周边,过程,周边地区,释放,地面,垂直分布,理化性质,沙尘暴,
 
 	//	time: 48.113 seconds
-	//	:盐碱尘暴,研究,艾比湖周边,过程,周边地区,释放,地面,垂直分布,理化性质,沙尘暴,
 
 	//	time: 0.031 seconds
 
