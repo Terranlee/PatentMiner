ea65be9de51d9564616c2f40035816d96db98b5f
terranlee <ltrthu@163.com>
2015-03-19 19:17:06 +0800

    merge code from zjq

--- src/keyword/KeywordExtract.java
+++ src/keyword/KeywordExtract.java
@@ -1,30 +1,67 @@
 package keyword;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import edu.thu.keyword.dao.DAO;
 import edu.thu.keyword.main.KeywordsExtracter;
 
 public class KeywordExtract {
-	public static void test() {
+	
+	private void extractKey(String infilename, String outfilename){
+		File infile = null;
+		File outfile = null;
+		BufferedReader reader = null;
+		BufferedWriter writer = null;
+		try{
+			infile = new File(infilename);
+			outfile = new File(outfilename);
+			reader = new BufferedReader(new FileReader(infile));
+			writer = new BufferedWriter(new FileWriter(outfile));
+			String content = "";
+			Integer round = 0;
+			while(true){
+				try {
+					content = reader.readLine();
+					if(content == null){
+						break;
+					}
+					String keyword = KeywordsExtracter.ExtractKey("", content, 5);
+					writer.write("文本: \n" + content + "\n关键词: \n" + keyword + "\n");
+					if(round % 100 == 0){
+						System.out.println("Round " + round.toString() + ": " + keyword);
+					}
+				}catch(Exception e){
+					System.out.println("Exception");
+					continue;
+				}finally{
+					round += 1;
+				}
+			}
+			}catch(Exception e){
+				System.out.println("Exception");
+			}finally{
+				try{
+					reader.close();
+					writer.close();
+				}catch(Exception e){
+					System.out.println("Exception");
+				}
+			}
+		}
+	
+	public static void main(String[] args) {
 		long start=System.currentTimeMillis();
-		
-		//KeywordsExtracter ek=new KeywordsExtracter();
-		String t = "艾比湖周边地区盐碱尘暴地面过程及其机理研究";
-		String c = "盐碱尘暴又称化学尘暴，由于具有化学污染、化学腐蚀和化学毒性，其危害比普通沙尘暴更为严重。目前，国内以野外观测与风洞实验相结合手段对盐碱尘暴过程的系统性深入研究基本空白。本项目拟通过实地观测、实验室模拟、分析测试等技术手段，研究艾比湖周边地区盐漠地表物质组成、理化性质、水-热-风等自然因子与盐碱尘暴的孕育关系，阐明盐漠地表盐碱尘的释放机理；测定不同类型盐漠表面盐碱尘释放强度，揭示盐碱尘释放过程的动力机制，建立盐碱尘释放强度与影响盐碱尘释放的主要因素之间的关系。观测分析盐碱尘暴输送的盐碱沙尘颗粒物在地面30米高度内的水平通量及垂直分布结构，建立通量梯度模型，并与其他区域沙尘暴研究结果进行对比；分析盐碱尘暴输送的盐碱沙尘的理化性质（化学成分、元素浓度、粒级配置和辐射性等）、垂直分布及其与尘源地盐漠物质理化特征的相关性。";
-		
-		System.out.println("check stopwords :" + DAO.isInStopwords("鎻愰珮"));
-		String keyword = KeywordsExtracter.ExtractKey(t,c,10);
-		System.out.println(":" + keyword);
+		System.out.println("check stopwords :" + DAO.isInStopwords("提高"));
+		KeywordExtract k = new KeywordExtract();
+		k.extractKey("patentdata/newenergy.txt", "patentdata/newenergy-key.txt");
+		k.extractKey("patentdata/metal.txt", "patentdata/metal-key.txt");
 		long end=System.currentTimeMillis();
 		int timeMilliseconds = (int) (end - start);
-		System.out.println("\n" + "time: " + (timeMilliseconds / 1000f)
-				+ " seconds");
-		
-		start = System.currentTimeMillis();
-		keyword = KeywordsExtracter.ExtractKey(t,c,10);
-		System.out.println(":" + keyword);
-		end=System.currentTimeMillis();
-		timeMilliseconds = (int) (end - start);
-		System.out.println("\n" + "time: " + (timeMilliseconds / 1000f)
-				+ " seconds");
+		System.out.println("\n" + "time: " + (timeMilliseconds / 1000f) + " seconds");
 	}
 }
