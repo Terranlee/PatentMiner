62964b12906c64068fc324d44d8e8576547ae7ab
Terranlee <ltrthu@163.com>
2015-03-19 19:29:44 -0700

    erase previews code

--- src/miner/Miner.java
+++ src/miner/Miner.java
@@ -5,11 +5,6 @@ import keyword.KeywordExtract;
 
 public class Miner {
 	public static void main(String[] args){
-		KeywordExtract.test();
-
-	//	time: 48.113 seconds
-
-	//	time: 0.031 seconds
 
 	}
 }
