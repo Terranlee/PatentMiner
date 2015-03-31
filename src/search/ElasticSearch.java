0d68b846ded29782deb9cda5ed11362f5405f713
JingqingZ <zhangjqsmiling@gmail.com>
2015-03-31 11:47:45 +0800

    test search time

--- src/search/ElasticSearch.java
+++ src/search/ElasticSearch.java
@@ -19,15 +19,18 @@ public class ElasticSearch {
 		long time_one = System.currentTimeMillis();
 		node = nodeBuilder().node();
 		client = node.client();
+		SearchResponse response;
 		long time_two = System.currentTimeMillis();
-		SearchResponse response = client.prepareSearch("newenergy").setTypes("split").setSearchType(SearchType.DFS_QUERY_AND_FETCH).setQuery(QueryBuilders.queryString("保温 加热")).setSize(20).execute().actionGet();
+		for(int i = 0 ; i < 100 ; i++){
+			response = client.prepareSearch("newenergy").setTypes("split").setSearchType(SearchType.DFS_QUERY_AND_FETCH).setQuery(QueryBuilders.queryString("保温 加热")).setSize(20).execute().actionGet();
+		}
 		long time_three = System.currentTimeMillis();
-		System.out.println(response.toString());
+		//System.out.println(response.toString());
 		long time_four = System.currentTimeMillis();
 		node.close();
 		long time_five = System.currentTimeMillis();
 		System.out.println("Initialize time: " + (time_two - time_one));
-		System.out.println("Search time: " + (time_three - time_two));
+		System.out.println("Search time: " + ((time_three - time_two)/100.0));
 		System.out.println("Print time: " + (time_four - time_three));
 		System.out.println("Close time: " + (time_five - time_four));
 	}
